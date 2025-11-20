fe8228a5cf7bef1870d7f21874d91831a072e51c
mahyar <zahiriniamahyar@gmail.com>
2025-11-21 02:54:35 +0330

    Add project files.

---
+++ src/BookShop.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShop.Web.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
