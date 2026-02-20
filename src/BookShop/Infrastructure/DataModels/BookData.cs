2b8812b7f0358712e8a387feee6ec10520cfade1
mahyar <zahiriniamahyar@gmail.com>
2026-02-21 00:35:41 +0330

    book data model

--- src/BookShop/Infrastructure/DataModels/BookData.cs
+++ src/BookShop/Infrastructure/DataModels/BookData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace BookShop.Infrastructure.DataModels
@@ -7,10 +8,14 @@ namespace BookShop.Infrastructure.DataModels
     public class BookData
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+
+        [MaxLength(50)]
+        public required string Name { get; set; }
+        [MaxLength(500)]
+        public required string Description { get; set; }
         public int Price { get; set; }
-        public string Author { get; set; }
+        [MaxLength(250)]
+        public required string Author { get; set; }
         public int Year { get; set; }
         public int Pages { get; set; }
     }
